0a658a7add3fb83597895a5acdcb8d35f88cd09b
Ang Li <ali36@mail.depaul.edu>
2020-11-01 22:55:57 -0600

    add about

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,29 @@
+import { AboutComponent } from './about/about.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 import { AirPortComponent } from './air-port/air-port.component';
+import { NavigationComponent } from './navigation/navigation.component';
+
+const routes: Routes = [
+  {path: 'airport', component: AirPortComponent},
+  {path: 'about', component: AboutComponent},
+  {path: '', redirectTo: '/airport', pathMatch: 'full'}
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    AirPortComponent
+    AirPortComponent,
+    NavigationComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
